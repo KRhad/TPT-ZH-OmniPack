a9c0873a7e0936001976a03351461d986a604dfe
jacob1 <jfu614@gmail.com>
2024-02-15 22:14:22 -0500

    tpt++/cracker1000: Make LDTC copy BIZR, BIZRG and BIZRG's wavelength too 708d543d291

--- src/simulation/elements/LDTC.cpp
+++ src/simulation/elements/LDTC.cpp
@@ -34,7 +34,7 @@ const int FLAG_KEEP_SEARCHING = 0x8;
 /* Returns true for particles that activate the special FILT color copying mode */
 bool phot_data_type(int rt)
 {
-	return rt == PT_FILT || rt == PT_PHOT || rt == PT_BRAY;
+	return rt == PT_FILT || rt == PT_PHOT || rt == PT_BRAY || rt == PT_BIZR || rt == PT_BIZRG || rt == PT_BIZRS;
 }
 
 /* Returns true for particles that start a ray search ("dtec" mode) */
