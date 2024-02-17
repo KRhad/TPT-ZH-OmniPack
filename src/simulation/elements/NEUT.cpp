KRhad/TPT-ZH-OmniPack

commit 166bb99e8e50ce9525bfd25a5a85c654431bc8ae

tpt++/LBPHacker: Remove BOUNDS_CHECK 52a491762495

src/simulation/elements/NEUT.cpp
@@ -40,13 +40,12 @@ int FIRE_update(UPDATE_FUNC_ARGS);
 
 int NEUT_update(UPDATE_FUNC_ARGS)
 {
-	int r, rx, ry;
 	int pressureFactor = 3 + (int)sim->air->pv[y/CELL][x/CELL];
-	for (rx=-1; rx<2; rx++)
-		for (ry=-1; ry<2; ry++)
-			if (BOUNDS_CHECK)
+	for (int rx = -1; rx <= 1; rx++)
+		for (int ry = -1; ry <= 1; ry++)
+			if (rx || ry)
 			{
-				r = pmap[y+ry][x+rx];
+				int r = pmap[y+ry][x+rx];
 				switch (TYP(r))
 				{
 				case PT_WATR:

src/simulation/elements/PCLN.cpp
@@ -18,18 +18,17 @@
 
 int PCLN_update(UPDATE_FUNC_ARGS)
 {
-	int r, rx, ry, rt;
 	if (parts[i].ctype<=0 || parts[i].ctype>=PT_NUM || !sim->elements[parts[i].ctype].Enabled)
-		for (rx=-1; rx<2; rx++)
-			for (ry=-1; ry<2; ry++)
-				if (BOUNDS_CHECK)
+		for (int rx = -1; rx <= 1; rx++)
+			for (int ry = -1; ry <= 1; ry++)
+				if (rx || ry)
 				{
-					r = photons[y+ry][x+rx];
+					int r = photons[y+ry][x+rx];
 					if (!r)
 						r = pmap[y+ry][x+rx];
 					if (!r)
 						continue;
-					rt = TYP(r);
+					int rt = TYP(r);
 					if (!(sim->elements[rt].Properties & PROP_CLONE) &&
 						!(sim->elements[rt].Properties & PROP_BREAKABLECLONE) &&
 				        rt != PT_SPRK && rt != PT_NSCN && 
@@ -46,8 +45,8 @@ int PCLN_update(UPDATE_FUNC_ARGS)
 		//create photons a different way
 		if (parts[i].ctype == PT_PHOT)
 		{
-			for (rx=-1; rx<2; rx++)
-				for (ry=-1; ry<2; ry++)
+			for (int rx = -1; rx <= 1; rx++)
+				for (int ry = -1; ry <= 1; ry++)
 					if (rx || ry)
 					{
 						int r = sim->part_create(-1, x+rx, y+ry, PT_PHOT);
@@ -55,7 +54,7 @@ int PCLN_update(UPDATE_FUNC_ARGS)
 						{
 							parts[r].vx = rx*3.0f;
 							parts[r].vy = ry*3.0f;
-							if (r>i)
+							if (r > i)
 							{
 								// Make sure movement doesn't happen until next frame, to avoid gaps in the beams of photons produced
 								parts[r].flags |= FLAG_SKIPMOVE;
@@ -66,8 +65,8 @@ int PCLN_update(UPDATE_FUNC_ARGS)
 		//create life a different way
 		else if (parts[i].ctype == PT_LIFE)
 		{
-			for (rx=-1; rx<2; rx++)
-				for (ry=-1; ry<2; ry++)
+			for (int rx = -1; rx <= 1; rx++)
+				for (int ry = -1; ry <= 1; ry++)
 				{
 					sim->part_create(-1, x+rx, y+ry, PT_LIFE, parts[i].tmp);
 				}

src/simulation/elements/PHOT.cpp
@@ -20,8 +20,6 @@ int FIRE_update(UPDATE_FUNC_ARGS);
 
 int PHOT_update(UPDATE_FUNC_ARGS)
 {
-	int r, rx, ry;
-	float rr, rrr;
 	if (!(parts[i].ctype&0x3FFFFFFF))
 	{
 		sim->part_kill(i);
@@ -31,11 +29,11 @@ int PHOT_update(UPDATE_FUNC_ARGS)
 		if (RNG::Ref().chance(1, 10))
 			FIRE_update(UPDATE_FUNC_SUBCALL_ARGS);
 
-	for (rx=-1; rx<2; rx++)
-		for (ry=-1; ry<2; ry++)
-			if (BOUNDS_CHECK)
+	for (int rx = -1; rx <= 1; rx++)
+		for (int ry = -1; ry <= 1; ry++)
+			if (rx || ry)
 			{
-				r = pmap[y+ry][x+rx];
+				int r = pmap[y+ry][x+rx];
 				if (!r)
 					continue;
 				if (TYP(r)==PT_ISOZ || TYP(r)==PT_ISZS)
@@ -45,7 +43,7 @@ int PHOT_update(UPDATE_FUNC_ARGS)
 						parts[i].vx *= 0.90f;
 						parts[i].vy *= 0.90f;
 						sim->part_create(ID(r), x+rx, y+ry, PT_PHOT);
-						rrr = RNG::Ref().between(0, 359) * M_PI / 180.0f;
+						float rrr = RNG::Ref().between(0, 359) * M_PI / 180.0f, rr;
 						if (TYP(r) == PT_ISOZ)
 							rr = RNG::Ref().between(128, 255) / 127.0f;
 						else

commit 0445ce03e6c79eb436ad94d10336e2dac982ce70

Fix improper for loops that don't check under themselves, broken in 166bb99e8e50

src/simulation/elements/NEUT.cpp
@@ -42,166 +42,167 @@ int NEUT_update(UPDATE_FUNC_ARGS)
 {
 	int pressureFactor = 3 + (int)sim->air->pv[y/CELL][x/CELL];
 	for (int rx = -1; rx <= 1; rx++)
+	{
 		for (int ry = -1; ry <= 1; ry++)
-			if (rx || ry)
+		{
+			int r = pmap[y+ry][x+rx];
+			switch (TYP(r))
 			{
-				int r = pmap[y+ry][x+rx];
-				switch (TYP(r))
+			case PT_WATR:
+				if (RNG::Ref().chance(3, 20))
+					part_change_type(ID(r), x+rx, y+ry, PT_DSTW);
+				//no break
+			case PT_ICEI:
+			case PT_SNOW:
+				parts[i].vx *= 0.995f;
+				parts[i].vy *= 0.995f;
+				break;
+			case PT_PLUT:
+				if (RNG::Ref().chance(pressureFactor, 1000))
 				{
-				case PT_WATR:
-					if (RNG::Ref().chance(3, 20))
-						part_change_type(ID(r), x+rx, y+ry, PT_DSTW);
-					//no break
-				case PT_ICEI:
-				case PT_SNOW:
-					parts[i].vx *= 0.995f;
-					parts[i].vy *= 0.995f;
-					break;
-				case PT_PLUT:
-					if (RNG::Ref().chance(pressureFactor, 1000))
+					if (RNG::Ref().chance(1, 3))
 					{
-						if (RNG::Ref().chance(1, 3))
+						sim->part_create(ID(r), x+rx, y+ry, RNG::Ref().chance(2, 3) ? PT_LAVA : PT_URAN);
+						parts[ID(r)].temp = MAX_TEMP;
+						if (parts[ID(r)].type == PT_LAVA)
 						{
-							sim->part_create(ID(r), x+rx, y+ry, RNG::Ref().chance(2, 3) ? PT_LAVA : PT_URAN);
-							parts[ID(r)].temp = MAX_TEMP;
-							if (parts[ID(r)].type == PT_LAVA)
-							{
-								parts[ID(r)].tmp = 100;
-								parts[ID(r)].ctype = PT_PLUT;
-							}
+							parts[ID(r)].tmp = 100;
+							parts[ID(r)].ctype = PT_PLUT;
 						}
-						else
-						{
-							sim->part_create(ID(r), x+rx, y+ry, PT_NEUT);
-							parts[ID(r)].vx = 0.25f*parts[ID(r)].vx + parts[i].vx;
-							parts[ID(r)].vy = 0.25f*parts[ID(r)].vy + parts[i].vy;
-						}
-						sim->air->pv[y/CELL][x/CELL] += 10.0f * CFDS; //Used to be 2, some people said nukes weren't powerful enough
-						FIRE_update(UPDATE_FUNC_SUBCALL_ARGS);
 					}
-					break;
-#ifdef SDEUT
-				case PT_DEUT:
-					if (RNG::Ref().chance(pressureFactor + 1 + (parts[ID(r)].life/100), 1000))
-					{
-						DeutExplosion(sim, parts[ID(r)].life, x+rx, y+ry, restrict_flt(parts[ID(r)].temp + parts[ID(r)].life*500.0f, MIN_TEMP, MAX_TEMP), PT_NEUT);
-						sim->part_kill(ID(r));
-					}
-					break;
-#else
-				case PT_DEUT:
-					if (RNG::Ref().chance(pressureFactor + 1, 1000))
+					else
 					{
 						sim->part_create(ID(r), x+rx, y+ry, PT_NEUT);
 						parts[ID(r)].vx = 0.25f*parts[ID(r)].vx + parts[i].vx;
 						parts[ID(r)].vy = 0.25f*parts[ID(r)].vy + parts[i].vy;
-						if (parts[ID(r)].life>0)
-						{
-							parts[ID(r)].life --;
-							parts[ID(r)].temp = restrict_flt(parts[ID(r)].temp + parts[ID(r)].life*17.0f, MIN_TEMP, MAX_TEMP);
-							pv[y/CELL][x/CELL] += 6.0f * CFDS;
-						}
-						else
-							sim->part_kill(ID(r));
 					}
-					break;
-#endif
-				case PT_GUNP:
-					if (RNG::Ref().chance(3, 200))
-						sim->part_change_type(ID(r), x+rx, y+ry, PT_DUST);
-					break;
-				case PT_DYST:
-					if (RNG::Ref().chance(3, 200))
-						sim->part_change_type(ID(r), x+rx, y+ry, PT_YEST);
-					break;
-				case PT_YEST:
-					sim->part_change_type(ID(r), x+rx, y+ry, PT_DYST);
-					break;
-				case PT_PLEX:
-					if (RNG::Ref().chance(3, 200))
-						sim->part_change_type(ID(r), x+rx, y+ry, PT_GOO);
-					break;
-				case PT_NITR:
-					if (RNG::Ref().chance(3, 200))
-						sim->part_change_type(ID(r), x+rx, y+ry, PT_DESL);
-					break;
-				case PT_PLNT:
-					if (RNG::Ref().chance(1, 20))
-						sim->part_create(ID(r), x+rx, y+ry, PT_WOOD);
-					break;
-				case PT_DESL:
-				case PT_OIL:
-					if (RNG::Ref().chance(3, 200))
-						sim->part_change_type(ID(r), x+rx, y+ry, PT_GAS);
-					break;
-				case PT_COAL:
-					if (RNG::Ref().chance(1, 20))
-						sim->part_create(ID(r), x+rx, y+ry, PT_WOOD);
-					break;
-				case PT_BCOL:
-					if (RNG::Ref().chance(1, 20))
-						sim->part_create(ID(r), x+rx, y+ry, PT_SAWD);
-					break;
-				case PT_DUST:
-					if (RNG::Ref().chance(1, 20))
-						sim->part_change_type(ID(r), x+rx, y+ry, PT_FWRK);
-					break;
-				case PT_EMBR:
-					if (parts[i].tmp == 1 && RNG::Ref().chance(1, 20))
-						sim->part_change_type(ID(r), x+rx, y+ry, PT_FWRK);
-					break;
-				case PT_FWRK:
-					if (RNG::Ref().chance(1, 20))
-						parts[ID(r)].ctype = PT_DUST;
-					break;
-				case PT_ACID:
-					if (RNG::Ref().chance(1, 20))
-						sim->part_create(ID(r), x+rx, y+ry, PT_ISOZ);
-					break;
-				case PT_TTAN:
-					if (RNG::Ref().chance(1, 20))
-					{
-						sim->part_kill(i);
-						return 1;
-					}
-					break;
-				case PT_EXOT:
-					if (RNG::Ref().chance(1, 20))
-						parts[ID(r)].life = 1500;
-					break;
-				case PT_RFRG:
-					if (RNG::Ref().chance(1, 2))
-						sim->part_create(ID(r), x+rx, y+ry, PT_GAS);
-					else
-						sim->part_create(ID(r), x+rx, y+ry, PT_CAUS);
-					break;
-				case PT_RSSS:
-					if (!rx && !ry)
-					{
-						int ct_under = parts[ID(r)].ctype;
-						int tmp_under = parts[ID(r)].tmp;
-
-						//If there's a correct ctype set, liquefy into it
-						if (ct_under > 0 && ct_under < PT_NUM)
-						{
-							sim->part_create(ID(r), x, y, ct_under);
-
-							//If there's a correct tmp set, use it for ctype
-							if (tmp_under > 0 && ct_under < PT_NUM)
-								parts[ID(r)].ctype = tmp_under;
-						}
-						else
-							sim->part_change_type(ID(r), x, y, PT_RSST); //Default to RSST if no ctype
-
-						sim->part_kill(i);
-
-						return 1;
-					}
-					break;
-				default:
-					break;
+					sim->air->pv[y/CELL][x/CELL] += 10.0f * CFDS; //Used to be 2, some people said nukes weren't powerful enough
+					FIRE_update(UPDATE_FUNC_SUBCALL_ARGS);
 				}
+				break;
+#ifdef SDEUT
+			case PT_DEUT:
+				if (RNG::Ref().chance(pressureFactor + 1 + (parts[ID(r)].life/100), 1000))
+				{
+					DeutExplosion(sim, parts[ID(r)].life, x+rx, y+ry, restrict_flt(parts[ID(r)].temp + parts[ID(r)].life*500.0f, MIN_TEMP, MAX_TEMP), PT_NEUT);
+					sim->part_kill(ID(r));
+				}
+				break;
+#else
+			case PT_DEUT:
+				if (RNG::Ref().chance(pressureFactor + 1, 1000))
+				{
+					sim->part_create(ID(r), x+rx, y+ry, PT_NEUT);
+					parts[ID(r)].vx = 0.25f*parts[ID(r)].vx + parts[i].vx;
+					parts[ID(r)].vy = 0.25f*parts[ID(r)].vy + parts[i].vy;
+					if (parts[ID(r)].life>0)
+					{
+						parts[ID(r)].life --;
+						parts[ID(r)].temp = restrict_flt(parts[ID(r)].temp + parts[ID(r)].life*17.0f, MIN_TEMP, MAX_TEMP);
+						pv[y/CELL][x/CELL] += 6.0f * CFDS;
+					}
+					else
+						sim->part_kill(ID(r));
+				}
+				break;
+#endif
+			case PT_GUNP:
+				if (RNG::Ref().chance(3, 200))
+					sim->part_change_type(ID(r), x+rx, y+ry, PT_DUST);
+				break;
+			case PT_DYST:
+				if (RNG::Ref().chance(3, 200))
+					sim->part_change_type(ID(r), x+rx, y+ry, PT_YEST);
+				break;
+			case PT_YEST:
+				sim->part_change_type(ID(r), x+rx, y+ry, PT_DYST);
+				break;
+			case PT_PLEX:
+				if (RNG::Ref().chance(3, 200))
+					sim->part_change_type(ID(r), x+rx, y+ry, PT_GOO);
+				break;
+			case PT_NITR:
+				if (RNG::Ref().chance(3, 200))
+					sim->part_change_type(ID(r), x+rx, y+ry, PT_DESL);
+				break;
+			case PT_PLNT:
+				if (RNG::Ref().chance(1, 20))
+					sim->part_create(ID(r), x+rx, y+ry, PT_WOOD);
+				break;
+			case PT_DESL:
+			case PT_OIL:
+				if (RNG::Ref().chance(3, 200))
+					sim->part_change_type(ID(r), x+rx, y+ry, PT_GAS);
+				break;
+			case PT_COAL:
+				if (RNG::Ref().chance(1, 20))
+					sim->part_create(ID(r), x+rx, y+ry, PT_WOOD);
+				break;
+			case PT_BCOL:
+				if (RNG::Ref().chance(1, 20))
+					sim->part_create(ID(r), x+rx, y+ry, PT_SAWD);
+				break;
+			case PT_DUST:
+				if (RNG::Ref().chance(1, 20))
+					sim->part_change_type(ID(r), x+rx, y+ry, PT_FWRK);
+				break;
+			case PT_EMBR:
+				if (parts[i].tmp == 1 && RNG::Ref().chance(1, 20))
+					sim->part_change_type(ID(r), x+rx, y+ry, PT_FWRK);
+				break;
+			case PT_FWRK:
+				if (RNG::Ref().chance(1, 20))
+					parts[ID(r)].ctype = PT_DUST;
+				break;
+			case PT_ACID:
+				if (RNG::Ref().chance(1, 20))
+					sim->part_create(ID(r), x+rx, y+ry, PT_ISOZ);
+				break;
+			case PT_TTAN:
+				if (RNG::Ref().chance(1, 20))
+				{
+					sim->part_kill(i);
+					return 1;
+				}
+				break;
+			case PT_EXOT:
+				if (RNG::Ref().chance(1, 20))
+					parts[ID(r)].life = 1500;
+				break;
+			case PT_RFRG:
+				if (RNG::Ref().chance(1, 2))
+					sim->part_create(ID(r), x+rx, y+ry, PT_GAS);
+				else
+					sim->part_create(ID(r), x+rx, y+ry, PT_CAUS);
+				break;
+			case PT_RSSS:
+				if (!rx && !ry)
+				{
+					int ct_under = parts[ID(r)].ctype;
+					int tmp_under = parts[ID(r)].tmp;
+
+					//If there's a correct ctype set, liquefy into it
+					if (ct_under > 0 && ct_under < PT_NUM)
+					{
+						sim->part_create(ID(r), x, y, ct_under);
+
+						//If there's a correct tmp set, use it for ctype
+						if (tmp_under > 0 && ct_under < PT_NUM)
+							parts[ID(r)].ctype = tmp_under;
+					}
+					else
+						sim->part_change_type(ID(r), x, y, PT_RSST); //Default to RSST if no ctype
+
+					sim->part_kill(i);
+
+					return 1;
+				}
+				break;
+			default:
+				break;
 			}
+		}
+	}
 	return 0;
 }
 

src/simulation/elements/PCLN.cpp
@@ -20,26 +20,27 @@ int PCLN_update(UPDATE_FUNC_ARGS)
 {
 	if (parts[i].ctype<=0 || parts[i].ctype>=PT_NUM || !sim->elements[parts[i].ctype].Enabled)
 		for (int rx = -1; rx <= 1; rx++)
+		{
 			for (int ry = -1; ry <= 1; ry++)
-				if (rx || ry)
+			{
+				int r = photons[y+ry][x+rx];
+				if (!r)
+					r = pmap[y+ry][x+rx];
+				if (!r)
+					continue;
+				int rt = TYP(r);
+				if (!(sim->elements[rt].Properties & PROP_CLONE) &&
+					!(sim->elements[rt].Properties & PROP_BREAKABLECLONE) &&
+					rt != PT_SPRK && rt != PT_NSCN &&
+					rt != PT_PSCN && rt != PT_STKM &&
+					rt != PT_STKM2)
 				{
-					int r = photons[y+ry][x+rx];
-					if (!r)
-						r = pmap[y+ry][x+rx];
-					if (!r)
-						continue;
-					int rt = TYP(r);
-					if (!(sim->elements[rt].Properties & PROP_CLONE) &&
-						!(sim->elements[rt].Properties & PROP_BREAKABLECLONE) &&
-				        rt != PT_SPRK && rt != PT_NSCN && 
-						rt != PT_PSCN && rt != PT_STKM && 
-						rt != PT_STKM2)
-					{
-						parts[i].ctype = TYP(r);
-						if (rt==PT_LIFE || rt==PT_LAVA)
-							parts[i].tmp = parts[ID(r)].ctype;
-					}
+					parts[i].ctype = TYP(r);
+					if (rt==PT_LIFE || rt==PT_LAVA)
+						parts[i].tmp = parts[ID(r)].ctype;
 				}
+			}
+		}
 	if (parts[i].ctype>0 && parts[i].ctype<PT_NUM && sim->elements[parts[i].ctype].Enabled && parts[i].life==10)
 	{
 		//create photons a different way

src/simulation/elements/PHOT.cpp
@@ -30,84 +30,85 @@ int PHOT_update(UPDATE_FUNC_ARGS)
 			FIRE_update(UPDATE_FUNC_SUBCALL_ARGS);
 
 	for (int rx = -1; rx <= 1; rx++)
+	{
 		for (int ry = -1; ry <= 1; ry++)
-			if (rx || ry)
+		{
+			int r = pmap[y+ry][x+rx];
+			if (!r)
+				continue;
+			if (TYP(r)==PT_ISOZ || TYP(r)==PT_ISZS)
 			{
-				int r = pmap[y+ry][x+rx];
-				if (!r)
-					continue;
-				if (TYP(r)==PT_ISOZ || TYP(r)==PT_ISZS)
+				if (RNG::Ref().chance(1, 400))
 				{
-					if (RNG::Ref().chance(1, 400))
-					{
-						parts[i].vx *= 0.90f;
-						parts[i].vy *= 0.90f;
-						sim->part_create(ID(r), x+rx, y+ry, PT_PHOT);
-						float rrr = RNG::Ref().between(0, 359) * M_PI / 180.0f, rr;
-						if (TYP(r) == PT_ISOZ)
-							rr = RNG::Ref().between(128, 255) / 127.0f;
-						else
-							rr = RNG::Ref().between(128, 255) / 127.0f;
-						parts[ID(r)].vx = rr*cosf(rrr);
-						parts[ID(r)].vy = rr*sinf(rrr);
-						sim->air->pv[y/CELL][x/CELL] -= 15.0f * CFDS;
-					}
-				}
-				else if (TYP(r) == PT_QRTZ || TYP(r) == PT_PQRT)
-				{
-					if (!ry && !rx)
-					{
-						float a = RNG::Ref().between(0, 359) * M_PI / 180.0f;
-						parts[i].vx = 3.0f*cosf(a);
-						parts[i].vy = 3.0f*sinf(a);
-						if (parts[i].ctype == 0x3FFFFFFF)
-							parts[i].ctype = 0x1F << RNG::Ref().between(0, 25);
-						if (parts[i].life)
-							parts[i].life++; //Delay death
-					}
-				}
-				else if (TYP(r) == PT_BGLA)
-				{
-					if (!ry && !rx)
-					{
-						float a = RNG::Ref().between(-50, 50) * 0.001f;
-						float rx = cosf(a), ry = sinf(a), vx, vy;
-						vx = rx * parts[i].vx + ry * parts[i].vy;
-						vy = rx * parts[i].vy - ry * parts[i].vx;
-						parts[i].vx = vx;
-						parts[i].vy = vy;
-					}
-				}
-				else if (TYP(r) == PT_RSST && !ry && !rx)//if on RSST, make it solid
-				{
-					int ct_under = parts[ID(r)].ctype;
-					int tmp_under = parts[ID(r)].tmp;
-
-					//If there's a correct ctype set, solidify RSST into it
-					if (ct_under > 0 && ct_under < PT_NUM)
-					{
-						sim->part_create(ID(r), x, y, ct_under);
-
-						//If there's a correct tmp set, use it for ctype
-						if (tmp_under > 0 && ct_under < PT_NUM)
-							parts[ID(r)].ctype = tmp_under;
-					}
+					parts[i].vx *= 0.90f;
+					parts[i].vy *= 0.90f;
+					sim->part_create(ID(r), x+rx, y+ry, PT_PHOT);
+					float rrr = RNG::Ref().between(0, 359) * M_PI / 180.0f, rr;
+					if (TYP(r) == PT_ISOZ)
+						rr = RNG::Ref().between(128, 255) / 127.0f;
 					else
-						sim->part_change_type(ID(r), x, y, PT_RSSS); //Default to RSSS if no ctype
-
-					sim->part_kill(i);
-
-					return 1;
-				}
-				else if (TYP(r) == PT_FILT)
-				{
-					if (parts[ID(r)].tmp == 9)
-					{
-						parts[i].vx += RNG::Ref().between(-500, 500) / 1000.0f;
-						parts[i].vy += RNG::Ref().between(-500, 500) / 1000.0f;
-					}
+						rr = RNG::Ref().between(128, 255) / 127.0f;
+					parts[ID(r)].vx = rr*cosf(rrr);
+					parts[ID(r)].vy = rr*sinf(rrr);
+					sim->air->pv[y/CELL][x/CELL] -= 15.0f * CFDS;
 				}
 			}
+			else if (TYP(r) == PT_QRTZ || TYP(r) == PT_PQRT)
+			{
+				if (!ry && !rx)
+				{
+					float a = RNG::Ref().between(0, 359) * M_PI / 180.0f;
+					parts[i].vx = 3.0f*cosf(a);
+					parts[i].vy = 3.0f*sinf(a);
+					if (parts[i].ctype == 0x3FFFFFFF)
+						parts[i].ctype = 0x1F << RNG::Ref().between(0, 25);
+					if (parts[i].life)
+						parts[i].life++; //Delay death
+				}
+			}
+			else if (TYP(r) == PT_BGLA)
+			{
+				if (!ry && !rx)
+				{
+					float a = RNG::Ref().between(-50, 50) * 0.001f;
+					float rx = cosf(a), ry = sinf(a), vx, vy;
+					vx = rx * parts[i].vx + ry * parts[i].vy;
+					vy = rx * parts[i].vy - ry * parts[i].vx;
+					parts[i].vx = vx;
+					parts[i].vy = vy;
+				}
+			}
+			else if (TYP(r) == PT_RSST && !ry && !rx)//if on RSST, make it solid
+			{
+				int ct_under = parts[ID(r)].ctype;
+				int tmp_under = parts[ID(r)].tmp;
+
+				//If there's a correct ctype set, solidify RSST into it
+				if (ct_under > 0 && ct_under < PT_NUM)
+				{
+					sim->part_create(ID(r), x, y, ct_under);
+
+					//If there's a correct tmp set, use it for ctype
+					if (tmp_under > 0 && ct_under < PT_NUM)
+						parts[ID(r)].ctype = tmp_under;
+				}
+				else
+					sim->part_change_type(ID(r), x, y, PT_RSSS); //Default to RSSS if no ctype
+
+				sim->part_kill(i);
+
+				return 1;
+			}
+			else if (TYP(r) == PT_FILT)
+			{
+				if (parts[ID(r)].tmp == 9)
+				{
+					parts[i].vx += RNG::Ref().between(-500, 500) / 1000.0f;
+					parts[i].vy += RNG::Ref().between(-500, 500) / 1000.0f;
+				}
+			}
+		}
+	}
 
 	return 0;
 }