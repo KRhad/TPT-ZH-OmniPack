8d0e1d51f434d428988c1c0ae4d783ace83e3d5c
jacob1 <jfu614@gmail.com>
2024-02-21 19:57:56 -0500

    Fix build number (version 56.0 build 160)

--- includes/defines.h
+++ includes/defines.h
@@ -42,7 +42,7 @@
 #define MOD_VERSION 56
 #define MOD_MINOR_VERSION 0
 #define MOD_SAVE_VERSION 27 //This is not the version number of my mod anymore, it's only changed when I change the saving code
-#define MOD_BUILD_VERSION 1 //For update checks
+#define MOD_BUILD_VERSION 160 //For update checks
 
 #define MOBILE_MAJOR 1
 #define MOBILE_MINOR 16
