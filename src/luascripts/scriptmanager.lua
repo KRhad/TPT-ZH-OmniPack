8175cc732db0437724821134be46208b4d45b8c6
jacob1 <jfu614@gmail.com>
2024-09-16 23:50:35 -0400

    Update to script manager v3.15 (scrolling fixes)

--- src/luascripts/scriptmanager.lua
+++ src/luascripts/scriptmanager.lua
@@ -1,6 +1,6 @@
 --Cracker64's Autorun Script Manager
 --The autorun to end all autoruns
---Version 3.14
+--Version 3.15
 
 --TODO:
 --manual file addition (that can be anywhere and any extension)
@@ -9,6 +9,7 @@
 --prettier, organize code
 
 --CHANGES:
+--Version 3.15: Bracket keys now scroll 5x faster, fix bracket scrolling being impossible on some keyboard layouts. Add ability to scroll by clicking and dragging, or with up/down arrow keys
 --Version 3.14: Fix extra newlines being inserted into scripts on Windows
 --Version 3.13: Better support for upcoming versions of TPT, all script downloads now async, settings now stored separately per scripts directory, fix another rare failure on startup
 --Version 3.12: Use https for all requests, online view loads async, add FILTER button to online, fix rare failure on startup if downloaded scripts list is corrupted
@@ -43,7 +44,7 @@ local icons = {
 	["delete2"] = "\xEE\x80\x86",
 	["folder"] = "\xEE\x80\x93"
 }
-if jacobsmod then
+if not evt then
 	icons = {
 		["delete1"] = "\133",
 		["delete2"] = "\134",
@@ -55,8 +56,8 @@ end
 if not socket then error("TPT version not supported") end
 if MANAGER then error("manager is already running") end
 
-local scriptversion = 16
-MANAGER = {["version"] = "3.14", ["scriptversion"] = scriptversion, ["hidden"] = true}
+local scriptversion = 17
+MANAGER = {["version"] = "3.15", ["scriptversion"] = scriptversion, ["hidden"] = true}
 
 local type = type -- people like to overwrite this function with a global a lot
 local TPT_LUA_PATH = 'scripts'
@@ -418,6 +419,8 @@ new = function(x,y,h,t,m)
 	bar.pos=0
 	bar.length=math.floor((1/math.ceil(bar.total-bar.numshown+1))*bar.h)
 	bar.soffset=math.floor(bar.pos*((bar.h-bar.length)/(bar.total-bar.numshown)))
+	bar.isClicked = false
+	bar.lastY = 0
 	function bar:update(total,shown,pos)
 		self.pos=pos or 0
 		if self.pos<0 then self.pos=0 end
@@ -442,16 +445,44 @@ new = function(x,y,h,t,m)
 		if y then self.y=self.y+y end
 	end)
 	function bar:process(mx,my,button,event,wheel)
-		if wheel~=0 and not MANAGER.hidden then
-			if self.total > self.numshown then
-				local previous = self.pos
-				self:move(wheel)
-				if self.pos~=previous then
-					return previous-self.pos
+		if self.total <= self.numshown then
+			return false
+		end
+		-- mousedown
+		if event == 1 then
+			if button == 1 then
+				self.isClicked = true
+				self.lastY = my
+			end
+		-- mouseup
+		elseif event == 2 then
+			self.isClicked = false
+		-- mousemove (scroll items if we're dragging)
+		elseif event == 3 then
+			if self.isClicked then
+				local diff = my - self.lastY
+				-- 8 is hardcoded height of each item so it works ...
+				if math.abs(diff) > 8 then
+					local previous = self.pos
+					if diff > 0 then
+						self:move(1)
+					else
+						self:move(-1)
+					end
+					self.lastY = my
+					return previous - self.pos
 				end
 			end
 		end
-		--possibly click the bar and drag?
+
+		-- mousewheel (scroll items)
+		if wheel~=0 and not MANAGER.hidden then
+			local previous = self.pos
+			self:move(wheel)
+			if self.pos~=previous then
+				return previous-self.pos
+			end
+		end
 		return false
 	end
 	return bar
@@ -490,7 +521,7 @@ new = function(x,y,w,h,f,text)
 	function b:process(mx,my,button,event,wheel)
 		local clicked = self.clicked
 		if event==2 then self.clicked = false end
-		if mx<self.x or mx>self.x2 or my<self.y or my>self.y2 then return false end
+		if mx<self.x or mx>self.x2 or my<self.y or my>self.y2 then self.clicked = false return false end
 		if event==1 then
 			self.clicked=true
 		elseif clicked then
@@ -653,6 +684,7 @@ new_button = function(x,y,w,h,splitx,f,f2,text,localscript)
 			end
 		else
 			if self.checkbut.canupdate then self.checkbut:process(mx,my,button,event,wheel) end
+			self.clicked = 0
 		end
 		return true
 	end
@@ -717,6 +749,7 @@ new = function(x,y,w,h)
 		if move==0 then return end
 		for i,check in ipairs(self.list) do
 			check:onmove(0,move)
+			check.clicked = 0
 		end
 	end
 	function box:process(mx,my,button,event,wheel)
@@ -805,7 +838,7 @@ new = function(x,y,w,h)
 		end
 	end)
 	function w:process(mx,my,button,event,wheel)
-		if mx<self.x or mx>self.x2 or my<self.y or my>self.y2 then if button == 0 then return end ui_button.sidepressed() return true end
+		if (mx<self.x or mx>self.x2 or my<self.y or my>self.y2) and event == 1 then ui_button.sidepressed() return true end
 		local ret
 		for i,sub in ipairs(self.sub) do
 			if sub:process(mx,my,button,event,wheel) then ret = true end
@@ -985,18 +1018,17 @@ local function mouseclick(mousex,mousey,button,event,wheel)
 	return false
 end
 local jacobsmod_old_menu_check = false
-local function keypress(key,nkey,modifier,event)
-	if jacobsmod and (key == 'o' or nkey == 96) and event == 1 then jacobsmod_old_menu_check = true end
-	if nkey==27 and not MANAGER.hidden then MANAGER.hidden=true return false end
+local function keypress(key, scan, rep, shift, ctrl, alt)
+	if jacobsmod and (scan == ui.SDL_SCANCODE_O) and not rep then jacobsmod_old_menu_check = true end
+	if scan == ui.SDL_SCANCODE_ESCAPE and not MANAGER.hidden then MANAGER.hidden=true return false end
 	if MANAGER.hidden then return end
 
-	if event == 1 then
-		if key == "[" then
-			mainwindow:process(mainwindow.x+30, mainwindow.y+30, 0, 2, 1)
-		elseif key == "]" then
-			mainwindow:process(mainwindow.x+30, mainwindow.y+30, 0, 2, -1)
-		end
+	if scan == ui.SDL_SCANCODE_LEFTBRACKET or scan == ui.SDL_SCANCODE_UP then
+		mainwindow:process(mainwindow.x+30, mainwindow.y+30, 0, 2, 5)
+	elseif scan == ui.SDL_SCANCODE_RIGHTBRACKET or scan == ui.SDL_SCANCODE_DOWN then
+		mainwindow:process(mainwindow.x+30, mainwindow.y+30, 0, 2, -5)
 	end
+
 	return false
 end
 --small button on right to bring up main menu
@@ -1437,4 +1469,4 @@ if started~="" then
 	MANAGER.print("Auto started"..started)
 end
 tpt.register_mouseevent(mouseclick)
-tpt.register_keypress(keypress)
+evt.register(evt.keypress, keypress)
