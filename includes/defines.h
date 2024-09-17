a2899d4990590a23953da8cc4defc161ddb9ac8c
jacob1 <jfu614@gmail.com>
2024-09-17 00:27:28 -0400

    Version 57.2

--- includes/defines.h
+++ includes/defines.h
@@ -40,9 +40,9 @@
 #define API_VERSION_MINOR 0 // always 0
 
 #define MOD_VERSION 57
-#define MOD_MINOR_VERSION 1
+#define MOD_MINOR_VERSION 2
 #define MOD_SAVE_VERSION 27 //This is not the version number of my mod anymore, it's only changed when I change the saving code
-#define MOD_BUILD_VERSION 165 //For update checks
+#define MOD_BUILD_VERSION 166 //For update checks
 
 #define MOBILE_MAJOR 1
 #define MOBILE_MINOR 18
