80c5895d0e1e6a67b1a097de43160cedb98ed440
jacob1 <jfu614@gmail.com>
2024-12-25 00:24:53 -0500

    TPT ver 99.0 / Android ver 1.19 / mod ver 58.1

--- includes/defines.h
+++ includes/defines.h
@@ -28,9 +28,9 @@
 #endif
 
 //VersionInfoStart
-#define SAVE_VERSION 98
-#define MINOR_VERSION 2
-#define BUILD_NUM 363
+#define SAVE_VERSION 99
+#define MINOR_VERSION 0
+#define BUILD_NUM 377
 //VersionInfoEnd
 #define FAKE_SAVE_VERSION 99
 #define FAKE_MINOR_VER 0
@@ -40,13 +40,13 @@
 #define API_VERSION_MINOR 0 // always 0
 
 #define MOD_VERSION 58
-#define MOD_MINOR_VERSION 0
+#define MOD_MINOR_VERSION 1
 #define MOD_SAVE_VERSION 27 //This is not the version number of my mod anymore, it's only changed when I change the saving code
-#define MOD_BUILD_VERSION 167 //For update checks
+#define MOD_BUILD_VERSION 168 //For update checks
 
 #define MOBILE_MAJOR 1
-#define MOBILE_MINOR 18
-#define MOBILE_BUILD 124
+#define MOBILE_MINOR 19
+#define MOBILE_BUILD 126
 
 #define IDENT_VERSION "G" //Change this if you're not Simon! It should be a single letter
 
