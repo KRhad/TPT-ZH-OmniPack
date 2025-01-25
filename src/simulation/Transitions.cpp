426e58a2cb00578cb8c75267e9bbae1897607e38
jacob1 <jfu614@gmail.com>
2025-01-25 00:14:52 -0500

    tpt++/savask: Prevent heat convection in liquids looking farther than 1px away 3da994c3b866 04dac4c02017

--- src/simulation/Transitions.cpp
+++ src/simulation/Transitions.cpp
@@ -20,10 +20,10 @@ bool Simulation::TransferHeat(int i, int t, int surround[8])
 	{
 		float convGravX, convGravY;
 		GetGravityField(x, y, -2.0f, -2.0f, convGravX, convGravY);
-		auto offsetX = int(std::round(convGravX + x));
-		auto offsetY = int(std::round(convGravY + y));
+		auto offsetX = std::clamp(int(std::round(convGravX + x)), x-1, x+1);
+		auto offsetY = std::clamp(int(std::round(convGravY + y)), y-1, y+1);
 		//some heat convection for liquids
-		if ((offsetX != x || offsetY != y) && offsetX >= 0 && offsetX < XRES && offsetY >= 0 && offsetY < YRES)
+		if (offsetX != x || offsetY != y)
 		{
 			r = pmap[offsetY][offsetX];
 			if (!(!r || parts[i].type != TYP(r)))
