e88e7c314c871362f27e5f77245094a83abdd749
jacob1 <jfu614@gmail.com>
2026-05-12 19:05:14 -0400

    tpt++/savask: Cap velocity in ACEL, FRAY, and RPEL 95b9d6713008 bfd1199b8d7e

--- src/simulation/elements/FRAY.cpp
+++ src/simulation/elements/FRAY.cpp
@@ -41,8 +41,9 @@ int FRAY_update(UPDATE_FUNC_ARGS)
 			
 						if (r && !(sim->elements[TYP(r)].Properties & TYPE_SOLID))
 						{
-							parts[ID(r)].vx += nxi*((parts[i].temp-273.15f)/10.0f);
-							parts[ID(r)].vy += nyi*((parts[i].temp-273.15f)/10.0f);
+							auto coef = (parts[i].temp - 273.15f) / 10.0f;
+							parts[ID(r)].vx = restrict_flt(parts[ID(r)].vx + nxi * coef, -MAX_VELOCITY, MAX_VELOCITY);
+							parts[ID(r)].vy = restrict_flt(parts[ID(r)].vy + nyi * coef, -MAX_VELOCITY, MAX_VELOCITY);
 						}
 					}
 				}
--- src/simulation/elements/RPEL.cpp
+++ src/simulation/elements/RPEL.cpp
@@ -32,8 +32,8 @@ int RPEL_update(UPDATE_FUNC_ARGS)
 			{
 				if (!parts[i].ctype || parts[i].ctype == parts[ID(r)].type)
 				{
-					parts[ID(r)].vx += isign<int>(rx)*((parts[i].temp-273.15f)/10.0f);
-					parts[ID(r)].vy += isign<int>(ry)*((parts[i].temp-273.15f)/10.0f);
+					parts[ID(r)].vx = restrict_flt(parts[ID(r)].vx + isign<int>(rx) * ((parts[i].temp - 273.15) / 10.0f), -MAX_VELOCITY, MAX_VELOCITY);
+					parts[ID(r)].vy = restrict_flt(parts[ID(r)].vy + isign<int>(ry) * ((parts[i].temp - 273.15) / 10.0f), -MAX_VELOCITY, MAX_VELOCITY);
 				}
 			}
 		}
