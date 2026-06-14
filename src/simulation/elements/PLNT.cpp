4e9b6f45a0d95961a90fe2936bce841f9dced071
jacob1 <jfu614@gmail.com>
2026-06-14 19:22:29 -0400

    tpt++/savask: Make SEED more gravity-sensitive 1fb67ffe8de5

--- src/simulation/elements/PLNT.cpp
+++ src/simulation/elements/PLNT.cpp
@@ -30,6 +30,15 @@ int detectDown(Simulation *sim, int x, int y)
 
 	sim->GetGravityField(x, y, 1.0f, 1.0f, pGravX, pGravY);
 
+	float gravLen = hypot(pGravX, pGravY);
+
+	// Normalize gravity
+	if (gravLen > 0)
+	{
+		pGravX /= gravLen;
+		pGravY /= gravLen;
+	}
+
 	int gravx = 1;
 	int gravy = 1;
 
